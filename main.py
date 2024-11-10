d6c27d6b1ff5040374a91ebe2e39f7afe6d00f59
Lutz Dietterich <lutzdietterich@googlemail.com>
2024-11-10 16:36:39 +0100

    adjusted prints and comments, set fan interval to 2 minutes and runtime to 30 seconds

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ def control_fan(temperature, min_temp=18, max_temp=22, min_duty=200):
 # Funktion zur Steuerung des Lüfters basierend auf Luftfeuchtigkeit und Temperaturbedingungen
 def control_humidity(temperature, humidity, humidity_threshold=60, min_temp=15, max_humidity_duty=154):
     if humidity > humidity_threshold and temperature >= min_temp:
-        print(f"Luftfeuchtigkeit {humidity}% über Schwellwert bei {temperature}°C. Lüfter für 20 Sekunden auf 15% Leistung.")
+        print(f"Luftfeuchtigkeit {humidity}% über Schwellwert bei {temperature}°C. Lüfter für 30 Sekunden auf 15% Leistung.")
         pwm_fan.duty(max_humidity_duty)  # Setze Duty Cycle auf 15%
-        time.sleep(20)  # Lüfter für 20 Sekunden laufen lassen
+        time.sleep(30)  # Lüfter für 30 Sekunden laufen lassen
         pwm_fan.duty(0)  # Lüfter ausschalten
-        print("Lüfter ausgeschaltet für 3 Minuten Wartezeit.")
-        time.sleep(180)  # Warte 3 Minuten
+        print("Lüfter ausgeschaltet für 2 Minuten Wartezeit.")
+        time.sleep(120)  # Warte 2 Minuten
     else:
         print(f"Keine Regelung notwendig. Luftfeuchtigkeit: {humidity}%, Temperatur: {temperature}°C")
 
